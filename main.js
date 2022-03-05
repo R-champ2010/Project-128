fecf6e7d9ecff2d12098e1e14ad8987df096326b
R-champ2010 <93424691+R-champ2010@users.noreply.github.com>
2022-03-05 13:19:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+sunflower="";
+bel="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+    sunflower=loadMusic("Post Malone Swae Lee  Sunflower SpiderMan Into the SpiderVerse.mp3");
+    bel=loadMusic("Imagine Dragons - Believer.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(700,600);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 700, 600);
+}
+
+
+function modelLoaded(){
+    console.log('Posenet is initialised');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.X;
+        leftWristY=results[0].pose.leftWrist.Y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.X;
+        rightWristY=results[0].pose.rightWrist.Y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+    }
+}
